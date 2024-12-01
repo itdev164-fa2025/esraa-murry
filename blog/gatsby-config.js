068826b48160cc22bfe15ced9702c0bb8eb77f4f
saroo2a <murrye2@gmatc.matc.edu>
2024-12-01 16:02:54 -0600

    Completed Activity 1

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -7,12 +7,17 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
-module.exports = {
+ module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Blog`,
+    description: ``,
+    author: `Esraa Murry`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    contact: {
+      name: `Esraa Murry`,
+      company: `Blogs Inc.`,
+      address: `PO Box 1234`
+    }
   },
   plugins: [
     `gatsby-plugin-image`,
