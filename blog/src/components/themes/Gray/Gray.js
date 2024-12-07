d07f14c958ea43e85698674859b55e475df35cde
saroo2a <murrye2@gmatc.matc.edu>
2024-12-07 02:52:36 -0600

    Fixed folder name casing to Gray

---
+++ blog/src/components/themes/Gray/Gray.js
@@ -0,0 +1,15 @@
+export const GrayTheme = {
+    header: {
+      background: '#333', // Dark gray background for the header
+      fontColor: 'white', // White font color for header links
+    },
+    content: {
+      background: '#f5f5f5', // Light gray background for the page content
+      fontColor: '#333', // Dark gray font color for the main content
+    },
+    footer: {
+      background: '#333', // Dark gray background for the footer
+      fontColor: 'white', // White font color for the footer
+    }
+  };
+  
